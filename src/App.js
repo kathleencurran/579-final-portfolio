7ad4f790f65d7262811ceee256927482a05d43e8
kathleencurran <89861708+kathleencurran@users.noreply.github.com>
2022-04-25 19:55:54 -0400

    fix 9

--- src/App.js
+++ src/App.js
@@ -24,13 +24,15 @@ function App() {
       <div>
         <Navbar className={classes.root} />
         <Routes>
-          <Route path="/579-final-portfolio/projects" element={<Projects />} />
-          <Route path="/579-final-portfolio/about-me" element={<About />} />
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/about-me" component={About} />
           <Route
             path="/579-final-portfolio/projects/huddle"
             element={<Huddle />}
           />
-          <Route path="/579-final-portfolio/" element={<Homepage />} />
+          <Route path="/" element={<Homepage />} />
         </Routes>
         <Footer />
       </div>
